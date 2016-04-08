48c122fb3b11635956b4272c3e511f55063bb4a2
Samuel Creshal <samuel@creshal.de>
2016-04-08 14:59:57 +0200

    List all possible actions in metadata so we can reuse them.
    Also document new list action which comes with a later patch.

--- yspave/metadata.py
+++ yspave/metadata.py
@@ -1,29 +1,46 @@
-# vim: set tabstop=8 colorcolumn=80:
+# vim: set tabstop=8 shiftwidth=8 colorcolumn=80:
 appname = 'yspave'
 appvers = '0.0.10'
 
 database_version = 3
 database_encoding = 'utf8'
 
+verbs = [
+	'new', 'add', 'del',
+	'imp', 'get', 'list',
+	'copy', 'edit', 'pwgen',
+	'migrate'
+]
+
 actions = '''scrypt based password manager.
 
 Actions:
 	new	Prompts for title/description and generates a new password
 		with supplied entropy (default: %i bit)
+
 	add	Prompts for title, description and password
+
 	edit	Edits an entry matching the query string
+
 	del	Deletes the entry with the supplied ID
+
 	get	Searches the DB for the supplied query string
 		Shows the whole database if no query is supplied, this can
 		take a while.
+
+	list	Same as get, but does not show passwords, only metadata.
+
 	copy	Same as 'get', uses `copy_command` on (selectable) query result.
 		Default: Calls xsel to copy the result to primary selection.
+
 	pwgen	Generate a random password with supplied entropy and print it
 		(default: %i bit)
+
 	import	Imports passwords from a CSV file.
 		Column order is title, description, password
 		The format is the default Excel/Calc format (comma separation,
 		quoting optional).
+
 	migrate	Re-syncs the database, migrating it to new database formats
 		and doing other maintenance if necessary.
 		This is also done automatically on all writing operations
